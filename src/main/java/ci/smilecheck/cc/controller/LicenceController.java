ec5450069be6ff7912adc45ef867eda4441f78fb
maikolfox <ahoueromeo@gmail.com>
2022-02-19 23:49:11 +0000

    initial commit

---
+++ src/main/java/ci/smilecheck/cc/controller/LicenceController.java
@@ -0,0 +1,15 @@
+package ci.smilecheck.cc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LicenceController {
+
+	
+	
+	@GetMapping("/")
+    public String index() {
+        return "Hello there! I'm running.";
+    }
+}
---
+++ src/test/java/ci/smilecheck/cc/SmilecheckApplicationTests.java
@@ -0,0 +1,13 @@
+//package ci.smilecheck.cc;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SmilecheckApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
